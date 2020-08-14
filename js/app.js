612d691fe92f13e0fc7c2926c130aa3f5427f546
SaraAshraf1 <42324727+SaraAshraf1@users.noreply.github.com>
2020-08-15 01:04:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,108 +1,101 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+	/**
+	 * 
+	 * Manipulating the DOM exercise.
+	 * Exercise programmatically builds navigation,
+	 * scrolls to anchors from navigation,
+	 * and highlights section in viewport upon scrolling.
+	 * 
+	 * Dependencies: None
+	 * 
+	 * JS Version: ES2015/ES6
+	 * 
+	 * JS Standard: ESlint
+	 * 
+	 */
 
-/**
- * Define Global Variables
- * 
-*/
-const navbar = document.querySelector('#navbar__list');
-const sections = document.querySelectorAll('section')
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-// check which element is active
-function getActiveElem() {
-    maxSection = sections[0];
-    minVal = 1000000;
-    for (item of sections) {
-        let bounding = item.getBoundingClientRect();
-        if (bounding.top > -300 & bounding.top < minVal) {
-            minVal = bounding.top;
-            maxSection = item;
-        };
-    };
-    return maxSection;
-};
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-function addSections() {
-    for (let item of sections) {
-        let section = document.createElement('li');
-        section.className = 'menu__link';
-        section.dataset.nav = item.id;
-        section.innerText = item.dataset.nav;
-        navbar.appendChild(section);
-    };
-};
-
-// Add class 'active' to section when near top of viewport
-function setActive () {
-    window.addEventListener('scroll', function (event) {
-        let section = getActiveElem();
-        section.classList.add('your-active-class');
-        // set other sections as inactive
-        for (let item of sections) {
-            if (item.id != section.id & item.classList.contains('your-active-class')) {
-                item.classList.remove('your-active-class');
-            }
-        }
-        // set corresponding header style
-        const active = document.querySelector('li[data-nav="' + section.id + '"]');
-        active.classList.add('active__link');
-        // remove from other headers
-        const headers = document.querySelectorAll('.menu__link');
-        for (let item of headers) {
-            console.log(item);
-            if (item.dataset.nav != active.dataset.nav & item.classList.contains('active__link')) {
-                item.classList.remove('active__link');
-            }
-        };
-    });
-};
-
-// Scroll to anchor ID using scrollTO event
-function scrollToClick() {
-    navbar.addEventListener('click', function (event) {
-        const clicked = document.querySelector('#' + event.target.dataset.nav)
-        clicked.scrollIntoView();
-    });
-};
+	/**
+	 * Define Global Variables
+	 * 
+	 */
+	 const sections = document.querySelectorAll('section')
+	 const navList= document.getElementById("navbar__list")
+	/**
+	 * End Global Variables
+	 * Start Helper Functions
+	 * 
+	 */
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
-// Build menu 
-addSections();
+	/**
+	 * End Helper Functions
+	 * Begin Main Functions
+	 * 
+	 */
 
-// Scroll to section on link click
-scrollToClick();
+	// build the nav
+	function addSections(){
+		var li;
+		for(let item of sections){
 
-// Set sections as active
-setActive();
+			li+=`<li data-nav="${item.id}" class="menu__link ">${item.dataset.nav}</li>`
+		};
+		navList.innerHTML=li
+	};
+
+	// Add class 'active' to section when near top of viewport
+	function setActive(section){
+		//get the active nav button
+		var active =document.querySelector(`[data-nav="${section.id}"]`)
+	   //set the active nav button as active
+	   active.classList.add('your-active-class');
+	   active.style.color='#fff'
+
+	   const headers =document.querySelectorAll('.menu__link')
+	 // set other nav buttons as inactive
+	 for(let item of headers)
+	 {
+	 	if (item.dataset.nav!=active.dataset.nav&item.classList.contains('your-active-class')){
+	 		item.classList.remove('your-active-class')
+	 		item.style.color='#000'
+	 	}
+	 }
+
+	        //set the active section as active
+	        section.classList.add('your-active-class');
+	           // set other sections as inactive
+	           for(let item of sections)
+	           {
+	           	if(item.id!=section.id & item .classList.contains('your-active-class')){
+	           		item.classList.remove('your-active-class')
+	           	}
+
+	           }
+
+
+	       }
+
+	// Scroll to anchor ID using scrollTO event
+	function scrollToClick(){
+		navList.addEventListener('click',function (event){
+			const clicked=document.querySelector('#'+event.target.dataset.nav)
+			clicked.scrollIntoView();
+			setActive(clicked);
+		});
+	}
+
+	/**
+	 * End Main Functions
+	 * Begin Events
+	 * 
+	 */
+
+	// Build menu 
+	addSections();
+
+	// Scroll to section on link click
+	scrollToClick();
+
+	// Set sections as active
+	//its in scrollToClick()
 
